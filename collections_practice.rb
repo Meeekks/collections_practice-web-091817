786049023fef8f405324159b61d4c9ac42bd5a14
Kenneth Jiang <k3nyyj@gmail.com>
2017-09-27 04:36:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+require 'pry'
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by do |ele|
+    ele.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |ele|
+    ele[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |ele|
+    ele[0] == "a"
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |ele|
+    sum += ele
+  end
+  sum
+end
+
+def add_s(array)
+  array.map do |ele|
+    if ele == array[1]
+      array[1]
+    else
+      ele = ele + "s"
+    end
+  end
+end
